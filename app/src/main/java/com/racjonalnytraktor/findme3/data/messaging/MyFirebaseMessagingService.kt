95f1f264fbbd6be4e1d6c205de3330736d94850d
KonradusPL <konradus2012@gmail.com>
2018-05-31 16:16:55 +0200

    added sending notification

---
+++ app/src/main/java/com/racjonalnytraktor/findme3/data/messaging/MyFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.racjonalnytraktor.findme3.data.messaging
+
+/**
+ * Created by Admin on 2018-05-31.
+ */
+class MyFirebaseMessagingService {
+}
---
+++ app/src/main/java/com/racjonalnytraktor/findme3/data/model/UpdateTokenRequest.kt
@@ -0,0 +1,7 @@
+package com.racjonalnytraktor.findme3.data.model
+
+/**
+ * Created by Admin on 2018-05-31.
+ */
+class UpdateTokenRequest {
+}
