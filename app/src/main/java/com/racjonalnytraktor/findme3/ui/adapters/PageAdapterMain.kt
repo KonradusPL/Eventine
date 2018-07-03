52631187f8f9fcc6dd654282dd174d0d13710780
KonradusPL <konradus2012@gmail.com>
2018-07-03 11:38:19 +0200

    added new logo to login page

--- app/src/main/java/com/racjonalnytraktor/findme3/ui/adapters/PageAdapterMain.kt
+++ app/src/main/java/com/racjonalnytraktor/findme3/ui/adapters/PageAdapterMain.kt
@@ -1,6 +1,7 @@
 package com.racjonalnytraktor.findme3.ui.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
@@ -8,6 +9,10 @@ import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
+import com.mikepenz.community_material_typeface_library.CommunityMaterial
+import com.mikepenz.google_material_typeface_library.GoogleMaterial
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.IIcon
 
 import com.racjonalnytraktor.findme3.R
 import com.racjonalnytraktor.findme3.ui.main.MainMvp
@@ -19,12 +24,15 @@ import kotlinx.android.synthetic.main.custom_tab.view.*
 
 class PageAdapterMain(fm: FragmentManager, val context: Context): FragmentPagerAdapter(fm) {
 
-    private val tabIcons = intArrayOf(
-            R.drawable.ic_home_black_24dp,
-            R.drawable.ic_add_black_24dp,
-            R.drawable.ic_group_add_black_24dp)
+    private val tabIcons = ArrayList<IIcon>()
 
     val tabNames = context.resources.getStringArray(R.array.tab_names_main)
+
+    init {
+        tabIcons.add(CommunityMaterial.Icon.cmd_account_multiple_outline)
+        tabIcons.add(CommunityMaterial.Icon.cmd_home_outline)
+        tabIcons.add(CommunityMaterial.Icon.cmd_plus)
+    }
 
 
     override fun getItem(position: Int): Fragment {
@@ -44,7 +52,11 @@ class PageAdapterMain(fm: FragmentManager, val context: Context): FragmentPagerA
     fun getTabView(position: Int) : View{
         val view = LayoutInflater.from(context).inflate(R.layout.custom_tab, null)
         view.tabTitle.text = tabNames[position]
-        view.tabIcon.setImageResource(tabIcons[position])
+        view.tabIcon.icon = IconicsDrawable(context)
+                .icon(tabIcons[position])
+                .color(Color.WHITE)
+                .sizeDp(20)
+
 
         when(position){
             0 -> setTabColor(true,view,context)
@@ -57,12 +69,12 @@ class PageAdapterMain(fm: FragmentManager, val context: Context): FragmentPagerA
     fun setTabColor(selected: Boolean, view: View, ctx: Context){
         when(selected){
             true -> {
-                view.tabIcon.setColorFilter(ContextCompat.getColor(ctx, R.color.colorPrimaryDark))
-                view.tabTitle.setTextColor(ctx.resources.getColor(R.color.colorPrimaryDark))
+               // view.tabIcon.setColorFilter(ContextCompat.getColor(ctx, R.color.colorPrimaryDark))
+                //view.tabTitle.setTextColor(ctx.resources.getColor(R.color.colorPrimaryDark))
             }
             false ->{
-                view.tabIcon.setColorFilter(ContextCompat.getColor(ctx,R.color.dimGrey))
-                view.tabTitle.setTextColor(ctx.resources.getColor(R.color.dimGrey))
+                //view.tabIcon.setColorFilter(ContextCompat.getColor(ctx,R.color.dimGrey))
+                //view.tabTitle.setTextColor(ctx.resources.getColor(R.color.dimGrey))
             }
 
         }
